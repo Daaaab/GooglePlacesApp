f8a3d32e08b3ea205976a9fb542ea3af6414e85b
jkuc <jkuc@future-processing.com>
2016-11-23 10:58:59 +0100

    app navigation

--- GooglePlacesApp/app/src/main/java/jkuc/futureprocessing/fp/googleplacesapp/MainActivity.java
+++ GooglePlacesApp/app/src/main/java/jkuc/futureprocessing/fp/googleplacesapp/MainActivity.java
@@ -1,13 +1,25 @@
 package jkuc.futureprocessing.fp.googleplacesapp;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import jkuc.futureprocessing.fp.googleplacesapp.R;
+import jkuc.futureprocessing.fp.googleplacesapp.map.presenter.MapPresenter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapPresenter presenter = new MapPresenter();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        presenter.setView(bottomNavigationView, getSupportFragmentManager());
     }
 }
---
+++ GooglePlacesApp/app/src/main/java/jkuc/futureprocessing/fp/googleplacesapp/map/presenter/MapPresenter.java
@@ -0,0 +1,53 @@
+package jkuc.futureprocessing.fp.googleplacesapp.map.presenter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import jkuc.futureprocessing.fp.googleplacesapp.R;
+import jkuc.futureprocessing.fp.googleplacesapp.general.Presenter;
+import jkuc.futureprocessing.fp.googleplacesapp.list.view.ListFragment;
+import jkuc.futureprocessing.fp.googleplacesapp.map.view.MapFragment;
+
+public class MapPresenter implements Presenter {
+
+    public void setView(BottomNavigationView bottomMenu, FragmentManager fm) {
+        setupNavigation(bottomMenu, fm);
+        openMapFragment(fm);
+    }
+
+    private void setupNavigation(BottomNavigationView bottomMenu, final FragmentManager fm) {
+        bottomMenu.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch(item.getItemId()) {
+                            case R.id.action_map:
+                                openMapFragment(fm);
+                                break;
+                            case R.id.action_list:
+                                openListFragment(fm);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+    }
+
+    private void openMapFragment(FragmentManager fm){
+        MapFragment mapFragment = new MapFragment();
+        fm.beginTransaction()
+          .replace(R.id.frame, mapFragment, mapFragment.getClass().getName())
+          .commit();
+    }
+
+    private void openListFragment(FragmentManager fm){
+        ListFragment listFragment = new ListFragment();
+        fm.beginTransaction()
+          .replace(R.id.frame, listFragment, listFragment.getClass().getName())
+          .commit();
+    }
+}
